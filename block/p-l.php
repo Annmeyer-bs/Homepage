d3ff827eff081e645a2abf844fa4e157eb506f8a
hanna <hannamyr.bd@gmail.com>
2021-03-18 14:31:58 +0200

    +

--- block/p-l.php
+++ block/p-l.php
@@ -64,7 +64,7 @@
 						<a href="">Dries van Noten</a>
 				</div>
 			</div>
-		</div>
+		
 	<div class="swiper-button-prev"></div>
 	<div class="swiper-button-next"></div>
 	<div class="swiper-pagination"></div>
