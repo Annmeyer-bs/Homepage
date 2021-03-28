62df01d9b78307e6fdc6a1bae782a18b49465df2
hanna <hannamyr.bd@gmail.com>
2021-03-28 20:56:14 +0300

    v.3.5

--- www/block/sh_filter.php
+++ www/block/sh_filter.php
@@ -24,7 +24,7 @@
 					<option value="3">Dolce & Gabbana</option>
 				</select>
 			</div>
-			<div class="col-2">
+			<div class="col-3">
 				<div class="row">
 			<div class="col-6">
 				<label for="">Size</label>
@@ -33,7 +33,18 @@
 				<label class="sizepink"	for="">Size	Guide</label>
 			</div>
 			</div>
+			<div class="row">
+			<div class="col-9">
 				<ul class="size">
+					<li><input type="checkbox" id="checkboxOne" value="XS"><label for="checkboxOne">XS</label></li>
+					<li><input type="checkbox" id="checkboxTwo" value="S" checked><label for="checkboxTwo">S</label></li>
+					<li><input type="checkbox" id="checkboxThree" value="M" checked><label for="checkboxThree">M</label></li>
+				</ul>
+				</div>
+				<div class="col-3">
+				<button type="button" class="btn btn-secondary dropdown-toggle dropdown-toggle-split" id="dropdownMenuReference" data-bs-toggle="dropdown" aria-expanded="false" data-bs-reference="parent">
+				</button>
+    <ul class="dropdown-menu	size" aria-labelledby="dropdownMenuReference">
 					<li><input type="checkbox" id="checkboxOne" value="XS"><label for="checkboxOne">XS</label></li>
 					<li><input type="checkbox" id="checkboxTwo" value="S" checked><label for="checkboxTwo">S</label></li>
 					<li><input type="checkbox" id="checkboxThree" value="M" checked><label for="checkboxThree">M</label></li>
@@ -41,26 +52,30 @@
 					<li><input type="checkbox" id="checkboxFive" value="XL"><label for="checkboxFive">XL</label></li>
 					<li><input type="checkbox" id="checkboxSix" value="2XL" checked><label for="checkboxSix">2XL</label></li>
 				</ul>
-				<select class="dropdown" aria-label="Default select example">
-				<ul class="size">
-					<li><input type="checkbox" id="checkboxOne" value="XS"><label for="checkboxOne">XS</label></li>
-					<li><input type="checkbox" id="checkboxTwo" value="S" checked><label for="checkboxTwo">S</label></li>
-					<li><input type="checkbox" id="checkboxThree" value="M" checked><label for="checkboxThree">M</label></li>
-					<li><input type="checkbox" id="checkboxFour" value="L"><label for="checkboxFour">L</label></li>
-					<li><input type="checkbox" id="checkboxFive" value="XL"><label for="checkboxFive">XL</label></li>
-					<li><input type="checkbox" id="checkboxSix" value="2XL" checked><label for="checkboxSix">2XL</label></li>
-				</ul>
-				</select>
+				</div>
+			</div>
 			</div>
 			<div class="col-2">
 				<label for="" class="form-label">Color</label>
+			<div class="row">
+			<div class="col-7">
 				<ul class="color">
 					<li><input type="checkbox"  id="checkboxblack" value="black" ><label id="colblack"	for="checkboxblack"></label></li>
+					<li><input type="checkbox"  id="checkbox127681" value="#127681" ><label	id="col127681" for="checkbox127681"></label></li>
+					<li><input type="checkbox" id="checkbox32E0C4" value="#32E0C4"><label id="col32E0C4"	for="checkbox32E0C4"></label></li>
+				</ul>
+				</div>
+				<div class="col-5">
+				<button type="button" class="btn btn-secondary dropdown-toggle dropdown-toggle-split" id="dropdownMenuReference" data-bs-toggle="dropdown" aria-expanded="false" data-bs-reference="parent">
+				</button>
+    <ul class="dropdown-menu	color" aria-labelledby="dropdownMenuReference">
+	<li><input type="checkbox"  id="checkboxblack" value="black" ><label id="colblack"	for="checkboxblack"></label></li>
 					<li><input type="checkbox"  id="checkbox127681" value="#127681" ><label	id="col127681" for="checkbox127681"></label></li>
 					<li><input type="checkbox" id="checkbox32E0C4" value="#32E0C4"><label id="col32E0C4"	for="checkbox32E0C4"></label></li>
 					<li><input type="checkbox"  id="checkboxred" value="red" ><label	id="colred" for="checkboxred"></label></li>
 					<li><input type="checkbox" id="checkboxyellow" value="yellow"><label id="colyellow"	for="checkboxyellow"></label></li>
 				</ul>
+				</div>
 			</div>
 		</div>
 	</div>
